23955d081997ff7d65540da2304acc90c35ff470
Alex <alexjcosta01@gmail.com>
2019-04-04 16:58:12 -0300

    Adding demo for Evosuite test generation tool.

---
+++ test-generation/evosuite/demos/basics/demo01/SumOperator.java
@@ -0,0 +1,9 @@
+package simpleproject;
+
+public class SumOperator {
+	
+	public int sum(int x, int y) {
+		return x + y;
+	}
+
+}
---
+++ test-generation/evosuite/demos/basics/demo01/main/Main.java
@@ -0,0 +1,13 @@
+package simpleproject.main;
+
+import simpleproject.SumOperator;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		SumOperator so = new SumOperator();
+		System.out.println("Return: " + so.sum(10, 10));
+	}
+
+}
