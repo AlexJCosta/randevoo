0fc8642d680ac38b0252411e5cd901c17b31cbf1
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2019-05-02 12:20:25 -0300

    working scripts

--- run-lattes-analysis.sh
+++ run-lattes-analysis.sh
@@ -5,4 +5,4 @@ PATH_TO_CLASSES="/target/classes"
 TIMEOUT=15
 SEED=412397
 
-./run-randoop-evosuite.sh ${PROJECT} ${PATH_TO_CLASSES}
+./run-randoop-evosuite.sh ${PROJECT} ${PATH_TO_CLASSES} ${TIMEOUT} ${SEED}
--- run-randoop-evosuite.sh
+++ run-randoop-evosuite.sh
@@ -2,22 +2,41 @@
 
 PROJECT=$1
 PATH_TO_CLASSES=$2
-TIMEOUT=#3
-SEED=#4
+TIMEOUT=$3
+SEED=$4
 
 ## this directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 RANDOOP_JAR=${DIR}/libs/randoop/randoop-all-4.1.1.jar
+EVOSUITE_JAR=${DIR}/libs/evosuite/evosuite-1.0.6.jar
 
+(cd ${DIR}/test_programs/${PROJECT};
 
-CLASSLIST="${DIR}/list-of-classes.txt"
-(cd ${DIR}/test_programs/${PROJECT}/${PATH_TO_CLASSES};
+ ## build project
+ mvn install
 
- find . -name "*.class" | sed 's/\.class//g' | sed 's/\.\///g' | sed 's/\//./g' > ${CLASSLIST}
+ ## generate classpath
+ mvn dependency:build-classpath -Dmdep.outputFile=cp.txt
+ CP=$(cat cp.txt)
+ rm cp.txt
 
- java -ea -cp .:${RANDOOP_JAR} randoop.main.Main gentests --randomseed=${SEED} --classlist=${CLASSLIST} --output-limit=${TIMEOUT} 
+ CLASSLIST="${DIR}/list-of-classes.txt"
+
+ ## entering maven-generated class directory
+ (cd target/classes;
+
+  ## generating list of classes
+  find . -name "*.class" | sed 's/\.class//g' | sed 's/\.\///g' | sed 's/\//./g' > ${CLASSLIST}
+  #echo "java -ea -cp $CP:${RANDOOP_JAR} randoop.main.Main gentests --randomseed=${SEED} --classlist=${CLASSLIST} --output-limit=${TIMEOUT}"
+  java -ea -cp .:$CP:$RANDOOP_JAR randoop.main.Main gentests --randomseed=${SEED} --classlist=${CLASSLIST} --output-limit=${TIMEOUT}
+
+  ## iterating through every class in the project
+  while IFS= read -r file
+  do
+      java -jar ${EVOSUITE_JAR} -generateSuite -Dsearch_budget=${TIMEOUT} -Dstopping_condition=MaxTime -class $file -projectCP .:$CP
+  done < "$CLASSLIST"
+  rm $CLASSLIST  
+ )
 
 )
-
-
