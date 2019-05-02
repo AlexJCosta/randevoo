7cf531b52a00875507bda5a635f36274c6eb9c46
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2019-05-02 11:47:18 -0300

    polishing script

---
+++ run-lattes-analysis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PROJECT="lattes-analysis"
+PATH_TO_CLASSES="/target/classes"
+TIMEOUT=15
+SEED=412397
+
+./run-randoop-evosuite.sh ${PROJECT} ${PATH_TO_CLASSES}
--- run-randoop-evosuite.sh
+++ run-randoop-evosuite.sh
@@ -2,16 +2,22 @@
 
 PROJECT=$1
 PATH_TO_CLASSES=$2
+TIMEOUT=#3
+SEED=#4
 
 ## this directory
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 RANDOOP_JAR=${DIR}/libs/randoop/randoop-all-4.1.1.jar
 
-(cd ${DIR}/test_programs/${PROJECT};
- 
- find....
+
+CLASSLIST="${DIR}/list-of-classes.txt"
+(cd ${DIR}/test_programs/${PROJECT}/${PATH_TO_CLASSES};
+
+ find . -name "*.class" | sed 's/\.class//g' | sed 's/\.\///g' | sed 's/\//./g' > ${CLASSLIST}
+
+ java -ea -cp .:${RANDOOP_JAR} randoop.main.Main gentests --randomseed=${SEED} --classlist=${CLASSLIST} --output-limit=${TIMEOUT} 
+
 )
 
-java -jar gentests --classlist=
 
