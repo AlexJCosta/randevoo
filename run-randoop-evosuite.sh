39ba662718c83263ac62d323d2aa4c6211771577
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2019-05-02 11:06:23 -0300

    minor changes in script

--- run-randoop-evosuite.sh
+++ run-randoop-evosuite.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+PROJECT=$1
+PATH_TO_CLASSES=$2
+
 ## this directory
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
 RANDOOP_JAR=${DIR}/libs/randoop/randoop-all-4.1.1.jar
+
+(cd ${DIR}/test_programs/${PROJECT};
+ 
+ find....
+)
+
 java -jar gentests --classlist=
 
