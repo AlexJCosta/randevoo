4a8e3f7c3ceaa9fb60d19f8a977b3bf66fdbd5b8
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2019-05-16 21:07:19 -0300

    simplifying and generalizing AST transformation

---
+++ junit-parser/src/FindSubsequencesVisitor.java
@@ -0,0 +1,96 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.body.VariableDeclarator;
+import japa.parser.ast.expr.AssignExpr;
+import japa.parser.ast.expr.Expression;
+import japa.parser.ast.expr.FieldAccessExpr;
+import japa.parser.ast.expr.MethodCallExpr;
+import japa.parser.ast.expr.VariableDeclarationExpr;
+import japa.parser.ast.stmt.AssertStmt;
+import japa.parser.ast.stmt.ExpressionStmt;
+import japa.parser.ast.stmt.Statement;
+import japa.parser.ast.stmt.TryStmt;
+import japa.parser.ast.visitor.VoidVisitorAdapter;
+
+class FindSubsequencesVisitor extends VoidVisitorAdapter<Void> {
+
+    public StringBuffer seqs = new StringBuffer();
+
+    ClassOrInterfaceDeclaration now_ClassOrInterfaceDeclaration;
+    MethodDeclaration now_MethodDeclaration;
+    boolean within = false;
+    
+    @Override
+    public void visit(ClassOrInterfaceDeclaration n, Void v) {
+        now_ClassOrInterfaceDeclaration = n;
+        super.visit(n, v);
+    }
+    
+    static String TEMPLATE = "public static %s %s_%s() throws Throwable {\n %s %s \n}\n";
+    @Override
+    public void visit(MethodDeclaration n, Void v) {
+        now_MethodDeclaration = n;
+        super.visit(n, v);
+        if (n.getBody()==null || n.getBody().getStmts()==null) return;
+        int counter = 0; // reset                      
+        StringBuffer prefix = new StringBuffer();
+        Map<String, String> varTypes = new HashMap<String,String>();
+        for (Statement stmt : n.getBody().getStmts()) {
+            if (stmt instanceof ExpressionStmt) {
+                Expression exp = ((ExpressionStmt) stmt).getExpression();
+                if (exp instanceof MethodCallExpr || exp instanceof AssignExpr) { // a.m();
+                    String name;
+                    if (exp instanceof MethodCallExpr) {
+                        MethodCallExpr mce = (MethodCallExpr) exp;
+                        if (mce.getScope() == null) {
+                            if (mce.getName().startsWith("assert")) continue;
+                            else throw new RuntimeException("Please, check this...");
+                        }
+                        name = mce.getScope().toString();
+                    } else {
+                        AssignExpr ase = (AssignExpr) exp;
+                        Expression e = ase.getTarget();
+                        if (!(e instanceof FieldAccessExpr)) throw new RuntimeException("Please, check this...");
+                        FieldAccessExpr fae = (FieldAccessExpr) e;
+                        name = fae.getScope().toString();
+                    }
+                    String type = varTypes.get(name);
+                    if (type == null) {
+                        System.out.println(exp);
+                        throw new RuntimeException("Please, check this...");
+                    }
+                    String closing = exp.toString() + "; \n return " + name + ";";
+                    String s = String.format(TEMPLATE, type, n.getName(), counter, prefix.toString(), closing);
+                    seqs.append(s);
+                    prefix.append(exp.toString()+ ";\n");
+                    counter++;
+                } else if (exp instanceof VariableDeclarationExpr) { // T v = e;
+                    VariableDeclarationExpr vde = (VariableDeclarationExpr) exp;
+                    if (vde.getVars().size() != 1) throw new RuntimeException("Please, check this...");
+                    VariableDeclarator vd = (VariableDeclarator) vde.getVars().get(0);
+                    varTypes.put(vd.getId().toString(), vde.getType().toString());
+                    // sequences
+                    String s = String.format(TEMPLATE, vde.getType().toString(), n.getName(), counter, prefix.toString(), "return " + vd.getInit() + ";");
+                    seqs.append(s);
+                    prefix.append(exp.toString() + ";\n");
+                    counter++;
+                } else {
+                    System.out.println(exp.getClass());
+                    throw new RuntimeException("Please, check this...");                    
+                }
+            } else if (stmt instanceof AssertStmt) {
+                //TODO: doing nothing; check if it would help
+            } else if (stmt instanceof TryStmt) {
+                //TODO: doing nothing; check if it would help
+            } else {
+                System.out.println(stmt);
+                System.out.println(stmt.getClass());
+                throw new RuntimeException("Please, check this...");                
+            }
+        }
+    }
+    
+}
--- junit-parser/src/Main.java
+++ junit-parser/src/Main.java
@@ -22,11 +22,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.jar.Attributes.Name;
 
 public class Main {
 
@@ -56,10 +57,17 @@ public class Main {
         }
         br.close();
 
-        /** merge each compilation unit and produce a single file **/
-        //TO CHECK: I am assuming that merging tests from different classes will not result in collision problems
+        /** created wrapper class containing factory methods **/
+        createWrapperClass(units);
 
-        // create new compilation unit and one class/type declaration                                                                                                                                                                                                                                                                                        
+    }
+
+    private static void createWrapperClass(List<CompilationUnit> units) throws IOException {
+        /** merge each compilation unit and produce a single file **/
+        // TO CHECK: I am assuming that merging tests from different classes will not
+        // result in collision problems
+
+        // create new compilation unit and one class/type declaration
         CompilationUnit compUnit = new CompilationUnit();
         compUnit.setPackage(new PackageDeclaration(ASTHelper.createNameExpr("fromevosuite")));
         ClassOrInterfaceDeclaration type = new ClassOrInterfaceDeclaration(ModifierSet.PUBLIC, false, "ToRandoop");
@@ -67,27 +75,28 @@ public class Main {
         ASTHelper.addTypeDeclaration(compUnit, type);
 
         /**
-         * traverse all tests looking for distinct import declarations and methods. 
+         * traverse all tests looking for distinct import declarations and methods.
          * 
-         * We can't create a single test with all EvoSuite tests because some tests access 
-         * protected methods and constructors, which are only accessible from classes defined 
-         * in specific packages. Consequently, we need to respect the same package structure.
+         * We can't create a single test with all EvoSuite tests because some tests
+         * access protected methods and constructors, which are only accessible from
+         * classes defined in specific packages. Consequently, we need to respect the
+         * same package structure.
          **/
         int counter = 0;
         Set<ImportDeclaration> importSet = new HashSet<ImportDeclaration>();
-        for (CompilationUnit unit: units) {
+        for (CompilationUnit unit : units) {
             List<TypeDeclaration> typeDeclarations = unit.getTypes();
             if (typeDeclarations.size() != 1) {
                 throw new RuntimeException("Please check. We assume there was only one type in each test class.");
             }
             ClassOrInterfaceDeclaration clazz = (ClassOrInterfaceDeclaration) typeDeclarations.get(0);
-            
+
             // add import
             importSet.addAll(unit.getImports());
             // import this type
-            String name = unit.getPackage().getName()+"."+clazz.getName();
+            String name = unit.getPackage().getName() + "." + clazz.getName();
             importSet.add(new ImportDeclaration(new NameExpr(name), false, false));
-             
+
             for (BodyDeclaration bd : clazz.getMembers()) {
                 MethodDeclaration md = (MethodDeclaration) bd;
                 // creating new method declaration
@@ -100,12 +109,11 @@ public class Main {
                 ASTHelper.addStmt(body, new ReturnStmt(call));
                 // attaching the new method to the newly created compilation unit
                 type.getMembers().add(newMD);
-            }             
-         }
+            }
+        }
         compUnit.setImports(new ArrayList<ImportDeclaration>(importSet));
 
         saveCompilationUnit(compUnit);
-
     }
 
     private static void saveCompilationUnit(CompilationUnit compUnit) throws IOException {
@@ -114,7 +122,6 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new FileWriter(fname+".java"));
         bw.write(compUnit.toString());
         bw.close();
-        
     }
 
     private static CompilationUnit refactorEvoSuiteTest(String fileName) throws FileNotFoundException, ParseException, IOException {
@@ -122,16 +129,30 @@ public class Main {
         CompilationUnit compUnit = JavaParser.parse(in);
         in.close();
 
+        /** find subsequences */
+        FindSubsequencesVisitor findSubsequencesVisitor = new FindSubsequencesVisitor();
+        compUnit.accept(findSubsequencesVisitor, null);
+        // visitor produces a string with factory methods
+        StringBuffer sb = findSubsequencesVisitor.seqs;
+        String temp = String.format("class Foo { %s }", sb.toString());
+        Reader reader = new StringReader(temp);
+        CompilationUnit fooCompUnit = JavaParser.parse(reader, false);
+        reader.close();
+        // hack -> replace original list methods with new ones  
+        ClassOrInterfaceDeclaration clazz = (ClassOrInterfaceDeclaration) compUnit.getTypes().get(0);
+        clazz.getMembers().clear();
+        clazz.getMembers().addAll(((ClassOrInterfaceDeclaration) fooCompUnit.getTypes().get(0)).getMembers());
+        
         /** replace variable names */
-        ReplaceVarNamesVisitor replaceVarNamesVisitor = new ReplaceVarNamesVisitor();
-        compUnit.accept(replaceVarNamesVisitor, null);
+//        ReplaceVarNamesVisitor replaceVarNamesVisitor = new ReplaceVarNamesVisitor();
+//        compUnit.accept(replaceVarNamesVisitor, null);
 
         /** factor subsequences AST **/
-        FactorSubsequencesVisitor factorVisitor = new FactorSubsequencesVisitor();
-        compUnit.accept(factorVisitor, null);
-        // delayed modifications on AST
-        factorVisitor.deleteNodes();
-        factorVisitor.addNodes();
+//        FactorSubsequencesVisitor factorVisitor = new FactorSubsequencesVisitor();
+//        compUnit.accept(factorVisitor, null);
+//        // delayed modifications on AST
+//        factorVisitor.deleteNodes();
+//        factorVisitor.addNodes();
         return compUnit;
     }
 
