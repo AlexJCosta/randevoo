7ac3fc7cf14adc513a4a38cdbb6093f4f3700691
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2019-06-07 14:11:40 -0300

    adjusting script

--- run-stats.sh
+++ run-stats.sh
@@ -10,7 +10,7 @@ function analyze() {
     LASTRUN=$(ls -d -t ${DIR}/output-tests/${ALGO}-${PROJECT}* | grep -vE "\-sub" | head -n 1)
     (cd $LASTRUN;
      #grep -f files_to_test.txt jacoco.csv  | awk -F "," '{print $2"."$3,$6,$7}' 
-     grep -f files_to_test.txt jacoco.csv  | awk -F "," 'BEGIN {SUM1=0;SUM2=0}; {SUM1=SUM1+$6;SUM2=SUM2+$7}; END {printf "uncovered=%.3f covered=%.3f coverage=%.3f\n", SUM1, SUM2, 100*SUM2/(SUM1+SUM2)}'
+     grep -f files_to_test.txt jacoco.csv  | awk -F "," 'BEGIN {SUM1=0;SUM2=0;SUM3=0;SUM4=0}; {SUM1=SUM1+$6;SUM2=SUM2+$7;SUM3=SUM3+$4;SUM4=SUM4+$5}; END {printf "  uncovered branches=%.3f covered branches=%.3f branch coverage=%.3f\n  uncovered instructions=%.3f covered instructions=%.3f instruction coverage=%.3f\n", SUM1, SUM2, 100*SUM2/(SUM1+SUM2), SUM3, SUM4, 100*SUM4/(SUM3+SUM4)}'
     )
     
 }
